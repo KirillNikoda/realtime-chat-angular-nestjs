02f617440d10420fe35ca978ae02eef1596b61af
Kirill <nikodakirill386@gmail.com>
2021-09-28 06:05:58 +0300

    feat:/add registration form

--- frontend/src/app/public/components/register/register.component.ts
+++ frontend/src/app/public/components/register/register.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
+import { UserService } from '../../services/user-service/user.service';
+import { CustomValidators } from '../../_helpers/custom-validators';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  form: FormGroup = new FormGroup(
+    {
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      username: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required]),
+      passwordConfirm: new FormControl(null, [Validators.required]),
+    },
+    {
+      validators: CustomValidators.passwordsMatching,
+    }
+  );
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) {}
 
-  ngOnInit(): void {
+  register() {
+    if (this.form.valid) {
+      this.userService
+        .create({
+          email: this.email.value,
+          password: this.username.value,
+          username: this.username.value,
+        })
+        .pipe(tap(() => this.router.navigateByUrl('/public/login')))
+        .subscribe();
+    }
   }
 
+  get email(): FormControl {
+    return this.form.get('email') as FormControl;
+  }
+
+  get username(): FormControl {
+    return this.form.get('username') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.form.get('password') as FormControl;
+  }
+
+  get passwordConfirm(): FormControl {
+    return this.form.get('passwordConfirm') as FormControl;
+  }
 }
